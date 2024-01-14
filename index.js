dffc22a20b53a0fe43d28ffe3bcc2adf3a600671
stanley <stanley.njuguna@student.moringaschool.com>
2024-01-14 10:41:56 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(data => renderBooks(data))
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -11,7 +17,6 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
